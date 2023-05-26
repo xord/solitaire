f22c0b65127885fa59549da394eabb3a3c9e9fd8
xordog <xordog@gmail.com>
2023-05-27 01:10:17 +0900

    delete main.rb

--- lib/rubysketch/solitaire.rb
+++ lib/rubysketch/solitaire.rb
@@ -1,22 +1,63 @@
 require 'forwardable'
 require 'json'
 
-require 'utils'
-require 'timer'
-require 'history'
-require 'score'
-require 'animation'
-require 'particle'
-require 'shake'
-require 'scene'
-require 'sound'
-require 'dialog'
-require 'transitions'
-require 'button'
+require 'rubysketch/solitaire/common/utils'
+require 'rubysketch/solitaire/common/timer'
+require 'rubysketch/solitaire/common/history'
+require 'rubysketch/solitaire/common/score'
+require 'rubysketch/solitaire/common/animation'
+require 'rubysketch/solitaire/common/particle'
+require 'rubysketch/solitaire/common/shake'
+require 'rubysketch/solitaire/common/scene'
+require 'rubysketch/solitaire/common/sound'
+require 'rubysketch/solitaire/common/dialog'
+require 'rubysketch/solitaire/common/transitions'
+require 'rubysketch/solitaire/common/button'
 
-require 'solitaire/card'
-require 'solitaire/places'
+require 'rubysketch/solitaire/card'
+require 'rubysketch/solitaire/places'
 
-require 'solitaire/background'
-require 'solitaire/start'
-require 'solitaire/klondike'
+require 'rubysketch/solitaire/background'
+require 'rubysketch/solitaire/start'
+require 'rubysketch/solitaire/klondike'
+
+
+using RubySketch
+
+
+SCREEN_WIDTH  = 375 # screen width
+SCREEN_HEIGHT = 667
+STATE_PATH    = 'state.json'
+SCORES_PATH   = 'scores.json'
+
+
+setup do
+  setTitle "Solitaire"
+  size SCREEN_WIDTH, SCREEN_HEIGHT
+  angleMode DEGREES
+  noStroke
+
+  $root = RootScene.new 'Root', Background.new, Start.new
+end
+
+draw do
+  fireTimers
+  drawShake
+  push { $root.draw }
+end
+
+mousePressed do
+  $root.mousePressed mouseX, mouseY, mouseButton
+end
+
+mouseReleased do
+  $root.mouseReleased mouseX, mouseY, mouseButton
+end
+
+mouseMoved do
+  $root.mouseMoved mouseX, mouseY, mouseX - pmouseX, mouseY - pmouseY
+end
+
+mouseDragged do
+  $root.mouseDragged mouseX, mouseY, mouseX - pmouseX, mouseY - pmouseY
+end
