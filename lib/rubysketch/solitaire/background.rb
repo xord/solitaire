0136e3ff44c52692b1b8ea96dbba54920d2dbf99
xordog <xordog@gmail.com>
2023-06-02 17:26:09 +0900

    space -> cosmic2 shader

--- lib/rubysketch/solitaire/background.rb
+++ lib/rubysketch/solitaire/background.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 using RubySketch
 
 
@@ -10,25 +11,26 @@ class Background < Scene
   end
 
   def types()
-    %i[checker space]
+    %i[checker cosmic2]
   end
 
   def nextType()
-    types[(types.index(@current) + 1) % types.size]
+    index = types.index(@current) || 0
+    types[(index + 1) % types.size]
   end
 
   def set(type)
+    type = types.first unless types.include?(type)
     case type
     when :checker
       @shader   = checker
       @canvas   = createGraphics width, height
       @uniforms = %i[iTime]
       @interval = 3
-    when :space
-      @shader   = space
-      @canvas   = createGraphics width, height, displayDensity
+    when :cosmic2
+      @shader   = cosmic2
+      @canvas   = createGraphics width, height#, displayDensity
       @uniforms = %i[iTime iResolution]
-      #@canvas.filter BLUR, 2
       @interval = 1
     end
     @current               = type
@@ -61,26 +63,59 @@ class Background < Scene
     END
   end
 
-  def space()
-    @space ||= createShader nil, <<~END.sub('0.010', '0.002').gsub('iMouse', 'vec2(0.)')
-      // https://www.shadertoy.com/view/XlfGRj
-      // Star Nest by Pablo Roman Andrioli
-      // License: MIT
+  def cosmic2()
+    @cosmic2 ||= createShader nil, <<~END.gsub('iMouse', 'vec2(0.)')
+      // Cosmic 2
+      // https://www.shadertoy.com/view/XllGzN
+
+      // Started as Star Nest by Pablo RomÃ¡n Andrioli
+      // Modifications by Beibei Wang and Huw Bowles.
+      // This content is under the MIT License.
+
+      // On reducing the spatial high frequency noise:
+
+      // We simply limit the size of each contribution of each iteration of the fractal using min():
+      // a += i > 7 ? min( 12.,abs(length(p)-pa)) : abs(length(p)-pa)
+      // The test on the iteration count is optional, we found that most of the problem noise is introduced in the later
+      // iterations so we found that keeping the original formula for the earlier iterations helps to retain the 'volume'
+      // without the noise.
+
+      // On reducing the temporal noise:
+
+      // This version has volume samples aligned along view space Z isolines. When the camera moves
+      // forwards, the samples are shifted towards the viewer, allowing the camera to move forward
+      // smoothly without aliasing (besides the high frequency speckle noise).
+
+      // However if the  camera were to rotate around its origin, the volume samples towards
+      // the sides of the image sweep in Z and aliasing would occur. To make this case work, the samples
+      // need to be arranged in concentric rings around the camera. However in this configuration
+      // there will be some aliasing at the sides of the screen when the camera moves forward,
+      // because the motion of the camera can no longer be compensated for completely - one can
+      // pull in the vert rings but they will move at different rates in Z
+
+      // I had similar issues in a different context and made some diagrams etc, see
+      // http://advances.realtimerendering.com/s2013/OceanShoestring_SIGGRAPH2013_Online.pptx
+      // And developed a fast realtime version of adaptive stationary sampling:
+      // https://www.shadertoy.com/view/XdBXWW
+
+      // Question - the derivative can be computed for "free" using dual numbers, as in https://www.shadertoy.com/view/Xd2GzR .
+      // The derivate may help to eliminate noise? Or perhaps the second derivate. It would be very interesting to see these
+      // derivatives rendered.
 
       #define iterations 17
       #define formuparam 0.53
 
-      #define volsteps 20
-      #define stepsize 0.1
+      #define volsteps 18
+      #define stepsize 0.050
 
       #define zoom   0.800
       #define tile   0.850
-      #define speed  0.010
+      #define speed  0.10
 
       #define brightness 0.0015
       #define darkmatter 0.300
-      #define distfading 0.730
-      #define saturation 0.850
+      #define distfading 0.760
+      #define saturation 0.800
 
       void mainImage( out vec4 fragColor, in vec2 fragCoord )
       {
@@ -88,39 +123,62 @@ class Background < Scene
         vec2 uv=fragCoord.xy/iResolution.xy-.5;
         uv.y*=iResolution.y/iResolution.x;
         vec3 dir=vec3(uv*zoom,1.);
-        float time=iTime*speed+.25;
+        float time=(iTime-3311.)*speed;
+
+        vec3 from=vec3(1.,.5,0.5);
+
+        vec3 forward = vec3(0.,0.,1.);
 
         //mouse rotation
-        float a1=.5+iMouse.x/iResolution.x*2.;
-        float a2=.8+iMouse.y/iResolution.y*2.;
-        mat2 rot1=mat2(cos(a1),sin(a1),-sin(a1),cos(a1));
-        mat2 rot2=mat2(cos(a2),sin(a2),-sin(a2),cos(a2));
+        float a1 = 0.3;//3.1415926 * (iMouse.x/iResolution.x-.5);
+        mat2 rot1 = mat2(cos(a1),sin(a1),-sin(a1),cos(a1));
+        float a2 = .6;//3.1415926 * (iMouse.y/iResolution.y-.5);
+        mat2 rot2 = mat2(cos(a2),sin(a2),-sin(a2),cos(a2));
         dir.xz*=rot1;
-        dir.xy*=rot2;
-        vec3 from=vec3(1.,.5,0.5);
-        from+=vec3(time*2.,time,-2.);
-        from.xz*=rot1;
-        from.xy*=rot2;
+        forward.xz *= rot1;
+        dir.yz*=rot1;
+        forward.yz *= rot1;
+
+        // pan (dodgy)
+        from += (iMouse.x/iResolution.x-.5)*vec3(-forward.z,0.,forward.x);
+
+        //zoom
+        float zooom = time;
+        from += forward* zooom;
+        float sampleShift = mod( zooom, stepsize );
+        float zoffset = -sampleShift;
+        sampleShift /= stepsize; // make from 0 to 1
 
         //volumetric rendering
-        float s=0.1,fade=1.;
+        float s=0.1;
         vec3 v=vec3(0.);
         for (int r=0; r<volsteps; r++) {
-          vec3 p=from+s*dir*.5;
+          vec3 p=from+(s+zoffset)*dir;// + vec3(0.,0.,zoffset);
           p = abs(vec3(tile)-mod(p,vec3(tile*2.))); // tiling fold
           float pa,a=pa=0.;
           for (int i=0; i<iterations; i++) {
             p=abs(p)/dot(p,p)-formuparam; // the magic formula
-            a+=abs(length(p)-pa); // absolute sum of average change
+            //p=abs(p)/max(dot(p,p),0.005)-formuparam; // another interesting way to reduce noise
+            float D = abs(length(p)-pa); // absolute sum of average change
+            a += i > 7 ? min( 12., D) : D;
             pa=length(p);
           }
-          float dm=max(0.,darkmatter-a*a*.001); //dark matter
+          //float dm=max(0.,darkmatter-a*a*.001); //dark matter
           a*=a*a; // add contrast
-          if (r>6) fade*=1.-dm; // dark matter, don't render near
-          //v+=vec3(dm,dm*.5,0.);
+          //if (r>3) fade*=1.-dm; // dark matter, don't render near
+          // brightens stuff up a bit
+          float s1 = s+zoffset;
+          // need closed form expression for this, now that we shift samples
+          float fade = pow(distfading,max(0.,float(r)-sampleShift));
           v+=fade;
-          v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
-          fade*=distfading; // distance fading
+
+          // fade out samples as they approach the camera
+          if( r == 0 )
+            fade *= 1. - sampleShift;
+          // fade in samples as they approach from the distance
+          if( r == volsteps-1 )
+            fade *= sampleShift;
+          v+=vec3(2.*s1,4.*s1*s1,16.*s1*s1*s1*s1)*a*brightness*fade; // coloring based on distance
           s+=stepsize;
         }
         v=mix(vec3(length(v)),v,saturation); //color adjust
