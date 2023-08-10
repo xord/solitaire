ca326839ba300673ad65361c80a7499a72a7155d
xordog <xordog@gmail.com>
2023-08-11 01:40:29 +0900

    shake and emit dusts always

--- lib/rubysketch/solitaire/klondike.rb
+++ lib/rubysketch/solitaire/klondike.rb
@@ -779,7 +779,6 @@ class Klondike < Scene
   def backToPlace(card, vel)
     return if vel.mag == 0
     vec = vel.dup.normalize * sqrt(vel.mag) / 10 * sqrt(card.count)
-    return if vec.mag < 3
     shakeScreen vector: vec
     emitDustOnEdges card, size: sqrt(vec.mag).then {|m| m..(m * 5)}
   end
